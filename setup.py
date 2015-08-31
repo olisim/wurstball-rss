d92cd931f659b441339df0e5e81354ff1a82b150
Jochen Breuer <breuer@dajool.com>
2015-08-31 17:18:03 +0200

    Added setup.py
    Added setup.py to install wurstball-rss as standalone script via
    cheese shop.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wurstball-rss',
+    version='0.1',
+    packages=find_packages(".", exclude=["*.tests", "*.tests.*", "tests.*", "tests", "demo.*"]),
+    scripts=['wurstball_rss'],
+    url='https://github.com/olisim/wurstball-rss/',
+    license='MIT',
+    author='Oliver Simon',
+    author_email='simon@dajool.com',
+    maintainer='Jochen Breuer',
+    maintainer_email='breuer@dajool.com',
+    install_requires=[],
+    description='A RSS feed generator for the front page of http://wurstball.de.',
+    keywords='wurstball rss fun',
+    platforms='any',
+)
