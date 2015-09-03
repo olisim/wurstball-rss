5f556283f06b96a1c7ae79adcb6f64c880c59396
Jochen Breuer <breuer@dajool.com>
2015-09-03 20:57:04 +0200

    Added dependencies to setup.py
    Dependencies where missing in the setup.py. wurstbal-rss needs
    lxml, requests, pytz and feedgen.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wurstball-rss',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(".", exclude=["*.tests", "*.tests.*", "tests.*", "tests", "demo.*"]),
     scripts=['wurstball_rss'],
     url='https://github.com/olisim/wurstball-rss/',
@@ -11,7 +11,7 @@ setup(
     author_email='simon@dajool.com',
     maintainer='Jochen Breuer',
     maintainer_email='breuer@dajool.com',
-    install_requires=[],
+    install_requires=['lxml', 'requests', 'pytz', 'feedgen'],
     description='A RSS feed generator for the front page of http://wurstball.de.',
     keywords='wurstball rss fun',
     platforms='any',
